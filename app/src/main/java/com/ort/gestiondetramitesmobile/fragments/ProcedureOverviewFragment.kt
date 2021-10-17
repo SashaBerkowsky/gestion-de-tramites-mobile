98b05aaef2f2153d65c435c5d87d8f5fae0fe745
Sasha Berkowsky <s.nberkowsky@gmail.com>
2021-10-17 18:14:01 -0300

    showErrorDialog function

--- app/src/main/java/com/ort/gestiondetramitesmobile/fragments/ProcedureOverviewFragment.kt
+++ app/src/main/java/com/ort/gestiondetramitesmobile/fragments/ProcedureOverviewFragment.kt
@@ -1,11 +1,16 @@
 package com.ort.gestiondetramitesmobile.fragments
 
+import android.app.Dialog
+import android.graphics.Color
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
 import com.ort.gestiondetramitesmobile.R
 import com.ort.gestiondetramitesmobile.viewmodels.ProcedureOverviewViewModel
 
@@ -29,5 +34,27 @@ class ProcedureOverviewFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(ProcedureOverviewViewModel::class.java)
         // TODO: Use the ViewModel
     }
+
+    private fun showErrorDialog(errorMsg: String){
+        val dialog = Dialog(requireContext())
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(true)
+        dialog.setContentView(R.layout.state_dialog)
+
+        var btnBack = dialog.findViewById<Button>(R.id.btnBack)
+        var txtTitle = dialog.findViewById<TextView>(R.id.txtTitle)
+        var txtDescription = dialog.findViewById<TextView>(R.id.txtDescription)
+
+        txtTitle.setTextColor(Color.RED)
+        txtDescription.text = errorMsg
+
+        btnBack.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
 
 }

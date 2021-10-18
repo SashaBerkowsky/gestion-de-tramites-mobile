fca34b7367d975d86a4e3ebb29ac8a7474f1770b
Sasha Berkowsky <s.nberkowsky@gmail.com>
2021-10-18 01:18:29 -0300

    small fixes for real now

--- app/src/main/java/com/ort/gestiondetramitesmobile/fragments/ProfileFragment.kt
+++ app/src/main/java/com/ort/gestiondetramitesmobile/fragments/ProfileFragment.kt
@@ -6,10 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.ort.gestiondetramitesmobile.R
 import com.ort.gestiondetramitesmobile.viewmodels.ProfileViewModel
 
 class ProfileFragment : Fragment() {
+
+    lateinit var v: View
+    lateinit var btnEditProfile: Button
+    lateinit var btnChangePassword: Button
 
     companion object {
         fun newInstance() = ProfileFragment()
@@ -21,13 +27,33 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.profile_fragment, container, false)
+
+        v= inflater.inflate(R.layout.profile_fragment, container, false)
+
+        btnEditProfile = v.findViewById(R.id.btnEditProfile)
+        btnChangePassword = v.findViewById(R.id.btnChangePassword)
+
+        return v
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onStart(){
+        super.onStart()
+
+        btnEditProfile.setOnClickListener {
+            val action = ProfileFragmentDirections.actionProfileFragmentToEditProfileFragment()
+            findNavController().navigate(action)
+        }
+
+        btnChangePassword.setOnClickListener {
+            val action = ProfileFragmentDirections.actionProfileFragmentToChangePasswordFragment()
+            findNavController().navigate(action)
+        }
     }
 
 }

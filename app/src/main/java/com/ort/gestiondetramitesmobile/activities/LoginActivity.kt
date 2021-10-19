958b7866533570f1f9951de1222cb2e21bc97491
Priscila Bey <pri.bey@gmail.com>
2021-10-19 02:45:03 -0400

    making firebase login work attp 1

--- app/src/main/java/com/ort/gestiondetramitesmobile/activities/LoginActivity.kt
+++ app/src/main/java/com/ort/gestiondetramitesmobile/activities/LoginActivity.kt
@@ -4,12 +4,13 @@ import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -18,35 +19,50 @@ import com.ort.gestiondetramitesmobile.R
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var googleSignInClient : GoogleSignInClient
     private lateinit var auth: FirebaseAuth
+    private lateinit var signInButton: Button
+    private companion object {
+        private const val TAG = "LoginActivity"
+        private const val RC_SIGN_IN = 4926
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        signInButton = findViewById(R.id.sign_in_button)
 
         // Configure Google Sign In
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            // .requestIdToken(getString(R.string.default_web_client_id))
+            .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
 
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        var googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // ESTO ME ESTA DANDO NULL Y NO SE PORQUEEEEEEEEEE
+        signInButton.setOnClickListener{
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
 
         auth = Firebase.auth
 
     }
 
-    /*override fun onStart() {
+    override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
         updateUI(currentUser)
     }
 
-    private fun signIn() {
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if(currentUser == null){
+            Log.w(TAG, "No user signed in")
+            return
+        }
+        startActivity(Intent(this, HomeActivity::class.java))
+        finish()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -82,5 +98,6 @@ class LoginActivity : AppCompatActivity() {
                     updateUI(null)
                 }
             }
-    }*/
+    }
+
 }
--- app/src/main/java/com/ort/gestiondetramitesmobile/fragments/LoginFragment.kt
+++ app/src/main/java/com/ort/gestiondetramitesmobile/fragments/LoginFragment.kt
@@ -1,27 +1,15 @@
 package com.ort.gestiondetramitesmobile.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.snackbar.Snackbar
 import com.ort.gestiondetramitesmobile.R
-import com.ort.gestiondetramitesmobile.models.User
-import com.ort.gestiondetramitesmobile.viewmodels.NewProcedureViewModel
 import android.widget.TextView
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.common.SignInButton
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-
-
-
-
-
 
 
 class LoginFragment : Fragment() {
@@ -31,14 +19,12 @@ class LoginFragment : Fragment() {
     }
 
     private lateinit var v: View
-    private lateinit var viewModel: NewProcedureViewModel
     private lateinit var btnLogin : Button
     private lateinit var btnGoogle : SignInButton
     private lateinit var txtGoogle : String
+    private lateinit var signUp : TextView
 
-    private lateinit var userList: MutableList<User>
-    private var i: Int = 0
-    private var lastUserId: Int = 0
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,6 +34,7 @@ class LoginFragment : Fragment() {
 
         btnLogin = v.findViewById(R.id.btn_login)
         btnGoogle = v.findViewById(R.id.sign_in_button)
+        signUp = v.findViewById(R.id.account2)
         txtGoogle = "Iniciar sesión con Google"
 
         setGooglePlusButtonText(btnGoogle,txtGoogle)
@@ -60,46 +47,22 @@ class LoginFragment : Fragment() {
         findNavController().navigate(action)
     }
 
+    private fun navToSignUpFragment() {
+        val action = LoginFragmentDirections.actionLoginFragmentToSignupFragment()
+        findNavController().navigate(action)
+    }
+
     override fun onStart() {
         super.onStart()
-
 
         btnLogin.setOnClickListener {
             navToHomeActivity()
 
-        /*       val searchedUser = findUser()
-            if(searchedUser == null) {
-                i += 1
-                Snackbar.make(binding.root, "Usuario agregado", Snackbar.LENGTH_LONG).show()
-            } else {
-                Snackbar.make(binding.root, "El usuario ya existe", Snackbar.LENGTH_LONG).show()
-            }
-        }*/
-
         }
 
-        /*  private fun findUser(): User? {
-
-        var idx = 0
-        var searchedUser : User? = null
-        var user : User?
-        userList = userDao?.loadAllPersons() as MutableList<User>
-
-        while ( idx < userList.size && searchedUser == null ) {
-            user = userList[idx]
-
-            if (user.name == binding.name.text.toString() && user.password == binding.password.text.toString()) {
-                searchedUser = user
-                Log.d("sU","Usuario encontrado. ID: " + searchedUser.id)
-            }
-            idx++
+        signUp.setOnClickListener {
+            navToSignUpFragment()
         }
-        return searchedUser
-    }
-
-    fun getUser(): User? {
-        return userDao?.loadPersonById(lastUserId)
-    }*/
     }
 
     private fun setGooglePlusButtonText(signInButton: SignInButton, buttonText: String?) {

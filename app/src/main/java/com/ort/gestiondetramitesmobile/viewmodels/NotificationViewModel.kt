22d83ccc0b2431245bf189cf321710bfa76b4d8e
Desiree <desi1888@hotmail.com>
2021-11-11 00:03:05 -0300

    param read

--- app/src/main/java/com/ort/gestiondetramitesmobile/viewmodels/NotificationViewModel.kt
+++ app/src/main/java/com/ort/gestiondetramitesmobile/viewmodels/NotificationViewModel.kt
@@ -30,7 +30,7 @@ class NotificationViewModel : ViewModel() {
 
     fun getNotificationList(userId: String) {
 
-        val response = repository.getNotificationList(userId)
+        val response = repository.getNotificationList(userId, false)
 
         response.enqueue(object : Callback<List<Notification>>{
             override fun onResponse(call: Call<List<Notification>>, response: Response<List<Notification>>) {
